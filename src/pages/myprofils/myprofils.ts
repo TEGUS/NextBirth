c1d1e7e409e0a26658c1d3b33c3433fbafa36f8f
Tegus <tegaurelien@hotmail.fr>
2018-12-16 14:01:18 +0100

    ..mes mises à jour

--- src/pages/myprofils/myprofils.ts
+++ src/pages/myprofils/myprofils.ts
@@ -23,7 +23,7 @@ export class MyprofilsPage {
               public navParams: NavParams) {
   }
   
-  ionViewWillLoad() {
+  ionViewWillEnter() {
     this.localStorage.getKey('session').then(next => {
       this.user = next.user;
     }, error => {
